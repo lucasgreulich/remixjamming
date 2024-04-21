a81bb264684a90ed659b2a4bf0ce71a8bf19790b
Lucas Greulich <lucas.g.greulich@gmail.com>
2024-04-21 14:59:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Remix jamming App
         </p>
         <a
           className="App-link"
